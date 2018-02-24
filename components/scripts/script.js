057c424cd9de80eb19cef59e518fc034b0d8f7d7
aljun m <aljunma20@gmail.com>
2018-02-24 10:18:53 +0800

    add all initial files summary
    add all initial files

---
+++ components/scripts/script.js
@@ -0,0 +1,6 @@
+$(function() {
+
+   var topoffset = 888888; //variable for scrolling effects
+   console.log(topoffset); 
+
+  }); //function
---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    jsValidate = require('gulp-jsvalidate'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    concat = require('gulp-concat'),
+    webserver = require('gulp-webserver');
+
+var outputBuilds,
+    sassOutputStyle, 
+    sassSources,
+    jsSrc;
+
+//env = 'production';
+env = 'development';
+
+if (env==='development') {
+  //outputDir = 'builds/development/';
+   outputBuilds = 'builds/development/';
+  //sassStyle = 'expanded';
+  sassOutputStyle = 'expanded';
+
+} else {
+  outputBuilds = 'builds/production/';
+  sassOutputStyle = 'compressed';
+}
+
+jsSrc = [
+  'components/scripts/jquery.slim.min.js',
+  'components/scripts/popper.js',
+  'components/scripts/bootstrap.js',
+  'components/scripts/script.js'
+];
+
+
+sassSources = ['components/sass/style.scss'];
+
+
+gulp.task('js', function() {
+  gulp.src(jsSrc)
+
+    .pipe(concat('script.js'))
+    //.pipe(browserify())
+    .on('error', gutil.log)
+    .pipe(gulpif(env === 'production', uglify()))
+    .pipe(gulp.dest(outputBuilds + 'js'))
+    .pipe(connect.reload())
+});
+
+
+gulp.task('javascript', function () {
+  console.log("Validate JavaScript");
+  return gulp.src("components/scripts/**.js")
+    .pipe(jsValidate());
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      css: outputBuilds + 'css',
+      style: sassOutputStyle
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest( outputBuilds + 'css'))
+    .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['components/sass/*.scss', 'components/sass/*/*.scss'], ['compass']);    
+    gulp.watch(jsSrc, ['js']);
+    gulp.watch( ['components/scripts/**.js'], ['javascript']);
+    gulp.watch([outputBuilds + '/**/*.html'], ['html']);
+});
+
+gulp.task('webserver', function() {
+    gulp.src(outputBuilds + '/')
+        .pipe(webserver({
+            port: 3000,
+            livereload: true,
+            livereloadport: 8283,
+            open: true
+        }));
+});
+
+gulp.task('html', function() {
+    //gulp.src('builds/development/*.html')
+    gulp.src(outputBuilds + '/**/*.html') 
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputBuilds)))
+    .pipe(connect.reload())
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'javascript', 'compass', 'webserver']);
